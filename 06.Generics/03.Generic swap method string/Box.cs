27bcada14ca13ea2b0df495d6182208c9a856d84
andixboya <andreyanboyadzhiev@gmail.com>
2019-06-27 14:50:54 +0300

    Commiting task 3 and 4 from Generic exercises

---
+++ 06.Generics/03.Generic swap method string/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01Box
+{
+    public class Box<T>
+    {
+
+        private readonly List<T> values;
+
+
+        public Box()
+        {
+            this.values = new List<T> { };
+        }
+
+        public IReadOnlyCollection<T> Values => this.values.AsReadOnly();
+
+        public void AddValue(T value)
+        {
+            this.values.Add(value);
+
+        }
+
+        public void SwapElements(int firstIndex, int secondIndex)
+        {
+            var firstElement = values[firstIndex];
+            values[firstIndex] = values[secondIndex];
+            values[secondIndex] = firstElement;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var value in this.values)
+            {
+
+                sb.AppendLine($"{value.GetType().FullName}: {value.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ 06.Generics/03.Generic swap method string/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P01Box
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string current = Console.ReadLine();
+
+                box.AddValue(current);
+            }
+
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            box.SwapElements(firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+
+
+
+        }
+    }
+}
