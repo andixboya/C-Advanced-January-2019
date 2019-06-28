616e43a5fb08a74e57aebf5d5711aa79437cf4b2
andixboya <andreyanboyadzhiev@gmail.com>
2019-06-28 22:53:03 +0300

    Committing first and second tasks from Iterators and Comparators exercises

---
+++ 07. Iterators_and_comparators/02. Collection/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            string input = Console.ReadLine();
+
+            var collection = new ListyIterator<string>();
+
+            while (input!="END")
+            {
+
+                string[] partitions = input
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = partitions[0];
+                try
+                {
+
+                    switch (command)
+                    {
+
+                        case "Create":
+                            
+                            string[] items = partitions.Skip(1).ToArray();
+
+                            collection = new ListyIterator<string>(items);
+
+                            break;
+
+                        case "Move":
+                            bool result = collection.Move();
+
+                            Console.WriteLine(result);
+
+                            break;
+                        case "HasNext":
+                            result = collection.HasNext();
+                            Console.WriteLine(result);
+                            break;
+
+                        case "Print":
+                            collection.Print();
+                            break;
+                        case "PrintAll":
+                            collection.PrintAll();
+                            break;
+
+                    }
+                }
+
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                input = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ 07. Iterators_and_comparators/1.Listy Iterator/ListyIterator.cs
@@ -0,0 +1,71 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+
+    {
+        private readonly List<T> items;
+
+        private int currentIndex;
+
+        public ListyIterator(params T [] items)
+        {
+            this.items = new List<T>(items);
+            this.Reset();
+        }
+
+        public IReadOnlyCollection<T> Items => this.items;
+
+        
+
+        public bool Move()
+        {
+            if (currentIndex <this.Items.Count-1)
+            {
+                currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (currentIndex+1==this.Items.Count-1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.items.Count==0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.items[currentIndex]);
+        }
+
+        private void Reset()
+        {
+            this.currentIndex = 0;
+        }
+
+        public static ListyIterator<T> Create(params T[] items)
+        {
+
+            var result = new ListyIterator<T>(items);
+
+            return result;
+
+        }
+
+    }
+}
